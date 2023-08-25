150098c963647368e6833263543d350bb5201152
SakunPanthi123 <spanthi863@rku.ac.in>
2023-08-26 02:59:24 +0530

    Constructed a gridview builder, Added a game loop, Built tetris mechanics, Added left/right key presses, Implemented collision mechanics, Added a scoreboard..

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,218 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Tetris(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Tetris extends StatefulWidget {
+  const Tetris({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Tetris> createState() => _TetrisState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TetrisState extends State<Tetris> {
+  List<int> leftEdge = [
+    0,
+    10,
+    20,
+    30,
+    40,
+    50,
+    60,
+    70,
+    80,
+    90,
+    100,
+    110,
+    120,
+    130,
+    140,
+    150,
+    160,
+    170
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  List<int> rightEdge = [
+    9,
+    19,
+    29,
+    39,
+    49,
+    59,
+    69,
+    79,
+    89,
+    99,
+    109,
+    119,
+    129,
+    139,
+    149,
+    159,
+    169,
+    179
+  ];
+  static List<List<int>> tetrisShapes = [
+    [-10, -9, -8, -7], // Shape 1
+    [-10, -9, -8, 2], // Shape 2
+    [-10, -9, 1, 2], // Shape 3
+    [-10, 0, 1, 11], // Shape 4
+    [-10, -9, 0, 10], // Shape 5
+    [-10, 0, 1, 2], // Shape 6
+    [-10, 0, 10, 11], // Shape 7
+    [-10, -9, -8, 0], // Shape 8
+    [-9, 0, 1, 10], // Shape 9
+    [-10, -9, 0, 1], // Shape 10
+  ];
+  late Timer timer;
+  bool isIncoming = true;
+  int score = 0;
+  List<int> incoming = tetrisShapes[Random().nextInt(tetrisShapes.length)];
+  List<int> filled = [];
+  List<int> endLine = [170, 171, 172, 173, 174, 175, 176, 177, 178, 179];
+
+  void gameStart() {
+    timer = Timer.periodic(Duration(milliseconds: 400), (limer) {
+      incoming = incoming.map((e) => e + 10).toList();
+      setState(() {});
+      for (int i = 0; i < 4; i++) {
+        if (endLine.contains(incoming[i]) ||
+            filled.contains(incoming[i] + 10)) {
+          filled.addAll(incoming);
+          score += 10;
+          int a = Random().nextInt(tetrisShapes.length);
+          incoming = tetrisShapes[a];
+        }
+      }
     });
   }
 
+  void gameStop() {
+    timer.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 10),
+                itemCount: 180,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(1),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        color:
+                            incoming.contains(index) || filled.contains(index)
+                                ? Colors.blue
+                                : Colors.black,
+                      ),
+                      child: Center(
+                        child: Text(
+                          index.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              'Score $score',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    bool stopper = false;
+                    for (int i = 0; i < 4; i++) {
+                      if (leftEdge.contains(incoming[i]) ||
+                          filled.contains(incoming[i] - 1)) {
+                        stopper = true;
+                      }
+                    }
+                    if (!stopper) {
+                      incoming = incoming.map((e) => e - 1).toList();
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Icon(Icons.keyboard_arrow_left_outlined),
+                  )),
+              SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    bool stopper = false;
+                    for (int i = 0; i < 4; i++) {
+                      if (rightEdge.contains(incoming[i]) ||
+                          filled.contains(incoming[i] + 1)) {
+                        stopper = true;
+                      }
+                    }
+                    if (!stopper) {
+                      incoming = incoming.map((e) => e + 1).toList();
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Icon(Icons.keyboard_arrow_right_outlined),
+                  )),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'btn1',
+            onPressed: () {
+              gameStart();
+            },
+            child: Icon(Icons.play_arrow),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            heroTag: 'btn2',
+            onPressed: () {
+              gameStop();
+            },
+            child: Icon(Icons.stop),
+          ),
+        ],
+      ),
     );
   }
 }
